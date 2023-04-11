9977f1ad0600586d475ea19377edd930f86baa77
Brennin <bkjoiner1@gmail.com>
2023-04-11 13:05:37 -0400

    load

--- server/routes.js
+++ server/routes.js
@@ -55,7 +55,7 @@ router.get('/reviews/meta', async (req, res) => {
   }
 })
 
-router.get('/loaderio-7091b22c4c1d87810600486a8e3aba56.txt', (req, res) => {
+router.get('/loaderio-087881e9227b136a358fa1d55489a32b.txt', (req, res) => {
   res.sendFile('../loaderio-087881e9227b136a358fa1d55489a32b (1).txt', { root: '/' });
 });
 
