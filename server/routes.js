32410a38c662aed9f5ccc798c9787b0e11107685
Brennin <bkjoiner1@gmail.com>
2023-04-11 12:40:04 -0400

    f

--- server/routes.js
+++ server/routes.js
@@ -56,7 +56,7 @@ router.get('/reviews/meta', async (req, res) => {
 })
 
 router.get('/loaderio-7091b22c4c1d87810600486a8e3aba56.txt', (req, res) => {
-  res.sendFile(__dirname + '/../loaderio-7091b22c4c1d87810600486a8e3aba56 (1).txt');
+  res.sendFile(__dirname + '/../loaderio-7091b22c4c1d87810600486a8e3aba56.txt');
 });
 
 router.get
